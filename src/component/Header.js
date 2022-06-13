85b32f31f61911b8a58eb4364d6420776e9be7a8
Kelvin Bush <kelybush@gmail.com>
2022-06-13 16:20:49 +0300

    set up profile route link

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Header = () => (
-  <div className="navbar">
+  <header className="navbar">
     <div className="logo">
       <img src={logo} className="logoImage" alt="spacehub logo" />
       <h1 className="logoText">Space Travellers&apos; Hub</h1>
     </div>
-  </div>
+    <nav>
+      <NavLink
+        style={({ isActive }) => ({
+          textDecoration: isActive ? 'underline' : 'none',
+        })}
+        className="nav__link"
+        to="/profile"
+      >
+        My Profile
+      </NavLink>
+    </nav>
+  </header>
 );
 
 export default Header;
