9b574754dd5c038531f1491d7958c87d83d2cc0d
Kelvin Bush <kelybush@gmail.com>
2022-06-13 16:27:18 +0300

    set up profile navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './component/Header';
 import './App.css';
+import MyProfile from './pages/MyProfile';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/profile" element={<MyProfile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
