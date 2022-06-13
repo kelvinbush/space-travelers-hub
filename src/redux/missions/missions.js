dd6f022186ee0d3f2203b7abf2da87daf1f9e287
Kelvin Bush <kelybush@gmail.com>
2022-06-13 17:48:38 +0300

    add missions reducer

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,14 +2,24 @@ import axios from 'axios';
 
 const baseUrl = 'https://api.spacexdata.com/v3/missions';
 
-const FETCH_MISSIONS = 'FETCH_MISSIONS';
+const FETCH_MISSIONS = 'missionstore/missions/FETCH_MISSIONS';
 
 const initialState = [];
+
 const fetchMissionAction = (mission) => ({
   type: FETCH_MISSIONS,
   payload: mission,
 });
-const fetchMissions = () => async (dispatch) => {
+export const fetchMissions = () => async (dispatch) => {
   const response = await axios.get(baseUrl);
   dispatch(fetchMissionAction(response.data));
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_MISSIONS:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
