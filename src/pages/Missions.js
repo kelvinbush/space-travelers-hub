eded802683eb16f3ccd76e16f55a840f974a0c2c
Kelvin Bush <kelybush@gmail.com>
2022-06-13 17:54:50 +0300

    provide global state

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 
 const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+
   useEffect(() => {
     console.log('Missions');
   }, []);
