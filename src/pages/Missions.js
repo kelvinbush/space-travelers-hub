31d7bc0dc22de6c25576534984b835420ed80f65
Kelvin Bush <kelybush@gmail.com>
2022-06-13 17:59:58 +0300

    fetch missions without re-render

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('Missions');
+    if (missions.length === 0) {
+      dispatch(fetchMissions());
+    }
+    console.log('Missions: ', missions);
   }, []);
   return (
     <div />
