bd198b246888c057ccee2bcca630b3957eb3cffa
Kelvin Bush <kelybush@gmail.com>
2022-06-14 07:33:30 +0300

    add logging middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import missionsReducer from './missions/missions';
 
 const rootReducer = combineReducers({
   missions: missionsReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 export default store;
