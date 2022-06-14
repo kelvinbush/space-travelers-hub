7c14ad2e3655e58a264afd83191fa652e227fa89
Kelvin Bush <kelybush@gmail.com>
2022-06-14 11:17:35 +0300

    set up join mission actions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -3,12 +3,18 @@ import axios from 'axios';
 const baseUrl = 'https://api.spacexdata.com/v3/missions';
 
 const FETCH_MISSIONS = 'missionstore/missions/FETCH_MISSIONS';
+const JOIN_MISSION = 'missionstore/missions/JOIN_MISSION';
 
 const initialState = [];
 
 const fetchMissionAction = (mission) => ({
   type: FETCH_MISSIONS,
   payload: mission,
+});
+
+export const joinMissionAction = (missionId) => ({
+  type: JOIN_MISSION,
+  payload: missionId,
 });
 
 export const fetchMissions = () => async (dispatch) => {
@@ -20,6 +26,13 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSIONS:
       return action.payload;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return { ...mission, reserved: true };
+        }
+        return mission;
+      });
     default:
       return state;
   }
