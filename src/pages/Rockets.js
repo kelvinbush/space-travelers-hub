8b2eae3e05d294806af0d3c7726ad0a1a9f609e9
Assad Isah <nottherealalanturing@gmail.com>
2022-06-14 06:36:08 +0100

    fix: minor code refactor

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
-  const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (rockets.length === 0) {
-      dispatch(fetchRockets());
-    }
+    dispatch(fetchRockets());
   }, []);
   return <div />;
 };
