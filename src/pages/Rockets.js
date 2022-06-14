5d461dae2dfd50453b6a98c6df10272d47b2d070
Assad Isah <nottherealalanturing@gmail.com>
2022-06-14 06:37:45 +0100

    feat: add selectors

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchRockets());
+    if (rockets.length === 0) {
+      dispatch(fetchRockets());
+    }
   }, []);
   return <div />;
 };
